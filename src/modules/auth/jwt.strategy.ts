0d88244987ccd98c1c9e42763c328bc2eac08458
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-12-11 20:50:20 +0200

    add method to jwt/strategy

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -1,20 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
 import { IJwtPayload } from '../users/interfaces/jwt-payload.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TokenEntity } from '../../database/entities/token.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectRepository(TokenEntity)
+    private readonly tokenRepository: Repository<TokenEntity>,
+  ) {
+    const jwtSecret = configService.get<string>('JWT_SECRET') || '';
+
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET') || '',
+      secretOrKey: jwtSecret,
     });
   }
-  /* eslint-disable @typescript-eslint/require-await */
+
   async validate(payload: IJwtPayload): Promise<IJwtPayload> {
+    const tokenEntity = await this.tokenRepository.findOne({
+      where: { jti: payload.jti, IsBlocked: false },
+      relations: ['user'],
+    });
+    if (!tokenEntity) {
+      throw new UnauthorizedException('Token is blocked or invalid');
+    }
     return payload;
   }
 }
