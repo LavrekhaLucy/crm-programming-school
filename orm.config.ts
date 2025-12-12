b1a7ce6d61a659719a9a5727e7170386ba97cb41
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-12-12 23:07:50 +0200

    add orm.config

---
+++ orm.config.ts
@@ -0,0 +1,18 @@
+import { ConfigService } from '@nestjs/config';
+import { EnvService } from './src/shared/env.service';
+import { DataSource } from 'typeorm';
+
+const configService = new ConfigService();
+const envService = new EnvService(configService);
+
+export default new DataSource({
+  type: envService.dbType as 'mysql',
+  host: envService.mysqlHost,
+  port: envService.mysqlPort,
+  username: envService.mysqlUser,
+  password: envService.mysqlPassword,
+  database: envService.mysqlDatabase,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  // migrations: [path.join(process.cwd(), 'src/database/migrations/*.ts')],
+});
