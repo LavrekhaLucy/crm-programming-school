0826464282bbdc30184ebf9c5cad7c870b21ef6c
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-12-13 13:52:49 +0200

    add changes to envService

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,26 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../../database/entities/user.entity';
 import { TokenEntity } from '../../database/entities/token.entity';
+import { SharedModule } from '../../shared/shared.module';
+import { EnvService } from '../../shared/env.service';
 
 @Module({
   imports: [
     ConfigModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
-      imports: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-        // signOptions: {
-        //   expiresIn: configService.get<number>('JWT_EXPIRATION_TIME'),
-        // },
+      imports: [SharedModule],
+      useFactory: (envService: EnvService) => ({
+        secret: envService.jwtSecret,
+        signOptions: {
+          expiresIn: envService.jwtExpirationTime,
+        },
       }),
-      inject: [ConfigService],
+      inject: [EnvService],
     }),
     TypeOrmModule.forFeature([UserEntity, TokenEntity]),
   ],
--- src/shared/env.service.ts
+++ src/shared/env.service.ts
@@ -10,26 +10,30 @@ export class EnvService {
   public readonly mysqlUser: string;
   public readonly mysqlPassword: string;
   public readonly mysqlRootPassword: string;
+
   public readonly jwtSecret: string;
   public readonly jwtExpirationTime: number;
   public readonly accessTokenExpirationTime: number;
   public readonly refreshTokenExpirationTime: number;
 
   constructor(private configService: ConfigService) {
-    this.dbType = configService.get<string>('TYPE') || 'mysql';
-    this.mysqlHost = configService.get<string>('MYSQL_HOST') || '';
-    this.mysqlPort = configService.get<number>('MYSQL_PORT', 3306);
-    this.mysqlDatabase = configService.get<string>('MYSQL_DATABASE') || '';
-    this.mysqlUser = configService.get<string>('MYSQL_USER') || '';
-    this.mysqlPassword = configService.get<string>('MYSQL_PASSWORD') || '';
+    this.dbType = this.configService.get<string>('TYPE') || 'mysql';
+    this.mysqlHost = this.configService.get<string>('MYSQL_HOST') || '';
+    this.mysqlPort = this.configService.get<number>('MYSQL_PORT', 3306);
+    this.mysqlDatabase = this.configService.get<string>(
+      'MYSQL_DATABASE',
+      'crm-programming-school',
+    );
+    this.mysqlUser = this.configService.get<string>('MYSQL_USER') || '';
+    this.mysqlPassword = this.configService.get<string>('MYSQL_PASSWORD') || '';
     this.mysqlRootPassword =
-      configService.get<string>('MYSQL_ROOT_PASSWORD') || '';
-    this.jwtSecret = configService.get<string>('JWT_SECRET') || '';
+      this.configService.get<string>('MYSQL_ROOT_PASSWORD') || '';
+    this.jwtSecret = this.configService.get<string>('JWT_SECRET') || '';
     this.jwtExpirationTime =
-      configService.get<number>('JWT_EXPIRATION_TIME') || 3600;
+      this.configService.get<number>('JWT_EXPIRATION_TIME') || 3600;
     this.accessTokenExpirationTime =
-      configService.get<number>('ACCESS_TOKEN_EXPIRATION_TIME') || 600;
+      this.configService.get<number>('ACCESS_TOKEN_EXPIRATION_TIME') || 600;
     this.refreshTokenExpirationTime =
-      configService.get<number>('REFRESH_TOKEN_EXPIRATION_TIME') || 1200;
+      this.configService.get<number>('REFRESH_TOKEN_EXPIRATION_TIME') || 1200;
   }
 }
