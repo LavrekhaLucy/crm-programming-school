84743bb8719b9f346b22747e135127a44bf6d373
LavrekhaLucy <lavrehaludmila@gmail.com>
2026-01-07 10:45:43 +0200

    added change to users.service.spec

--- src/modules/users/tests/users.service.spec.ts
+++ src/modules/users/tests/users.service.spec.ts
@@ -31,112 +31,129 @@ describe('UsersService', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
+  describe('create', () => {
+    it('should create a new user', async () => {
+      jest.spyOn(repository, 'create').mockReturnValue(mockUserEntity);
+      repository.save.mockResolvedValue(mockUserEntity);
 
-  it('should create a new user', async () => {
-    jest.spyOn(repository, 'create').mockReturnValue(mockUserEntity);
-    repository.save.mockResolvedValue(mockUserEntity);
+      const result = await service.create(mockBaseUserReqDto);
 
-    const result = await service.create(mockBaseUserReqDto);
-
-    expect(repository.create).toHaveBeenCalledWith(mockBaseUserReqDto);
-    expect(repository.save).toHaveBeenCalledWith(mockUserEntity);
-    expect(result).toHaveProperty('id', 1);
-    expect(result).toEqual(mockUserEntity);
-  });
-  it('should find a user by id', async () => {
-    repository.findOne.mockResolvedValue(mockUserEntity);
-
-    const result = await service.findById(1);
-    expect(repository.findOne).toHaveBeenCalledWith({
-      where: { id: 1 },
-      relations: ['orders'],
+      expect(repository.create).toHaveBeenCalledWith(mockBaseUserReqDto);
+      expect(repository.save).toHaveBeenCalledWith(mockUserEntity);
+      expect(result).toHaveProperty('id', 1);
+      expect(result).toEqual(mockUserEntity);
     });
-    expect(result).toEqual(mockUserEntity);
   });
-  it('should find all users', async () => {
-    repository.find.mockResolvedValue([mockUserEntity]);
 
-    const result = await service.findAll();
+  describe('findById', () => {
+    it('should find a user by id', async () => {
+      repository.findOne.mockResolvedValue(mockUserEntity);
 
-    expect(repository.find).toHaveBeenCalledWith({
-      select: ['id', 'name', 'surname', 'email'],
-      order: { surname: 'ASC' },
+      const result = await service.findById(1);
+      expect(repository.findOne).toHaveBeenCalledWith({
+        where: { id: 1 },
+        relations: ['orders'],
+      });
+      expect(result).toEqual(mockUserEntity);
     });
-    expect(result).toEqual([mockUserEntity]);
   });
-  it('should update a user', async () => {
-    repository.update.mockResolvedValue(undefined);
-    repository.findOneBy.mockResolvedValue(mockUserEntity);
 
-    const result = await service.update(1, { name: 'UpdatedName' });
+  describe('findAll', () => {
+    it('should find all users', async () => {
+      repository.find.mockResolvedValue([mockUserEntity]);
 
-    expect(repository.update).toHaveBeenCalledWith(
-      { id: 1 },
-      { name: 'UpdatedName' },
-    );
-    expect(repository.findOneBy).toHaveBeenCalledWith({ id: 1 });
-    expect(result).toEqual(mockUserEntity);
+      const result = await service.findAll();
+
+      expect(repository.find).toHaveBeenCalledWith({
+        select: ['id', 'name', 'surname', 'email'],
+        order: { surname: 'ASC' },
+      });
+      expect(result).toEqual([mockUserEntity]);
+    });
   });
-  it('should disable a user', async () => {
-    const disabledUser = { ...mockUserEntity, isActive: false } as UserEntity;
-    repository.findOneBy.mockResolvedValue(mockUserEntity);
-    repository.save.mockResolvedValue(disabledUser);
 
-    const result = await service.disable(1);
+  describe('update', () => {
+    it('should update a user', async () => {
+      repository.update.mockResolvedValue(undefined);
+      repository.findOneBy.mockResolvedValue(mockUserEntity);
 
-    expect(repository.findOneBy).toHaveBeenCalledWith({ id: 1 });
-    expect(repository.save).toHaveBeenCalledWith(disabledUser);
-    expect(result).toEqual(mockUserResDto);
+      const result = await service.update(1, { name: 'UpdatedName' });
+
+      expect(repository.update).toHaveBeenCalledWith(
+        { id: 1 },
+        { name: 'UpdatedName' },
+      );
+      expect(repository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+      expect(result).toEqual(mockUserEntity);
+    });
   });
-  it('should throw NotFoundException when disabling a non-existing user', async () => {
-    repository.findOneBy.mockResolvedValue(null);
+  describe('disable', () => {
+    it('should disable a user', async () => {
+      const disabledUser = { ...mockUserEntity, isActive: false } as UserEntity;
+      repository.findOneBy.mockResolvedValue(mockUserEntity);
+      repository.save.mockResolvedValue(disabledUser);
 
-    await expect(service.disable(999)).rejects.toThrow('User #999 not found');
+      const result = await service.disable(1);
 
-    expect(repository.findOneBy).toHaveBeenCalledWith({ id: 999 });
-    expect(repository.save).not.toHaveBeenCalled();
+      expect(repository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+      expect(repository.save).toHaveBeenCalledWith(disabledUser);
+      expect(result).toEqual(mockUserResDto);
+    });
+
+    it('should throw NotFoundException when disabling a non-existing user', async () => {
+      repository.findOneBy.mockResolvedValue(null);
+
+      await expect(service.disable(999)).rejects.toThrow('User #999 not found');
+
+      expect(repository.findOneBy).toHaveBeenCalledWith({ id: 999 });
+      expect(repository.save).not.toHaveBeenCalled();
+    });
   });
-  it('should enable a user', async () => {
-    const enabledUser = { ...mockUserEntity, isActive: true } as UserEntity;
-    repository.findOneBy.mockResolvedValue(mockUserEntity);
-    repository.save.mockResolvedValue(enabledUser);
+  describe('enable', () => {
+    it('should enable a user', async () => {
+      const enabledUser = { ...mockUserEntity, isActive: true } as UserEntity;
+      repository.findOneBy.mockResolvedValue(mockUserEntity);
+      repository.save.mockResolvedValue(enabledUser);
 
-    const result = await service.enable(1);
+      const result = await service.enable(1);
 
-    expect(repository.findOneBy).toHaveBeenCalledWith({ id: 1 });
-    expect(repository.save).toHaveBeenCalledWith(enabledUser);
-    expect(result).toEqual(mockUserResDto);
+      expect(repository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+      expect(repository.save).toHaveBeenCalledWith(enabledUser);
+      expect(result).toEqual(mockUserResDto);
+    });
+    it('should throw NotFoundException when enabling a non-existing user', async () => {
+      repository.findOneBy.mockResolvedValue(null);
+
+      await expect(service.enable(999)).rejects.toThrow('User #999 not found');
+
+      expect(repository.findOneBy).toHaveBeenCalledWith({ id: 999 });
+      expect(repository.save).not.toHaveBeenCalled();
+    });
   });
-  it('should throw NotFoundException when enabling a non-existing user', async () => {
-    repository.findOneBy.mockResolvedValue(null);
+  describe('delete', () => {
+    it('should delete a user', async () => {
+      const deleteResult: DeleteResult = {
+        raw: [],
+        affected: 1,
+      };
 
-    await expect(service.enable(999)).rejects.toThrow('User #999 not found');
+      repository.delete.mockResolvedValue(deleteResult);
 
-    expect(repository.findOneBy).toHaveBeenCalledWith({ id: 999 });
-    expect(repository.save).not.toHaveBeenCalled();
-  });
-  it('should delete a user', async () => {
-    const deleteResult: DeleteResult = {
-      raw: [],
-      affected: 1,
-    };
+      await expect(service.delete(1)).resolves.not.toThrow();
 
-    repository.delete.mockResolvedValue(deleteResult);
+      expect(repository.delete).toHaveBeenCalledWith(1);
+    });
+    it('should throw NotFoundException when deleting a non-existing user', async () => {
+      const deleteResult: DeleteResult = {
+        raw: [],
+        affected: 0,
+      };
 
-    await expect(service.delete(1)).resolves.not.toThrow();
+      repository.delete.mockResolvedValue(deleteResult);
 
-    expect(repository.delete).toHaveBeenCalledWith(1);
-  });
-  it('should throw NotFoundException when deleting a non-existing user', async () => {
-    const deleteResult: DeleteResult = {
-      raw: [],
-      affected: 0,
-    };
+      await expect(service.delete(999)).rejects.toThrow('User #999 not found');
 
-    repository.delete.mockResolvedValue(deleteResult);
-
-    await expect(service.delete(999)).rejects.toThrow('User #999 not found');
-
-    expect(repository.delete).toHaveBeenCalledWith(999);
+      expect(repository.delete).toHaveBeenCalledWith(999);
+    });
   });
 });
