62c3ff684172f635108ee502ceaad42d2f2f81a0
LavrekhaLucy <lavrehaludmila@gmail.com>
2026-01-15 05:53:05 +0200

    moved files to folder backend

--- backend/src/modules/orders/order.module.ts
+++ backend/src/modules/orders/order.module.ts
@@ -4,10 +4,11 @@ import { OrderEntity } from '../../database/entities/order.entity';
 import { OrdersService } from './services/order.service';
 import { OrdersController } from './order.controller';
 import { UsersModule } from '../users/users.module';
+import { OrdersRepository } from '../repository/services/orders.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([OrderEntity]), UsersModule],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
   controllers: [OrdersController],
   exports: [OrdersService],
 })
