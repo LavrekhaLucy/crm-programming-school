2e601c4d036268488b59e55a3e999c6a02d8396d
LavrekhaLucy <lavrehaludmila@gmail.com>
2026-01-16 05:45:22 +0200

    add folder frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+const App = () => {
+    const [orders, setOrders] = useState([]);
+
+
+    useEffect(() => {
+        axios.get('/api/orders')
+            .then(({data}) => {
+                setOrders(data);
+            })
+    }, []);
+
+    console.log('FRONTEND VERSION 123');
+
+    return(
+            <div>
+                {
+                    orders.map(order => <div key={order.id}> {JSON.stringify(order)}</div>)
+                }
+                <h1>Checking</h1>
+            </div>
+    )
+};
+export {App};
