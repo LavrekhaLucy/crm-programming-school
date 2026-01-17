f565f7f993b2e2fbefe6accc91a0d6859eb85f66
LavrekhaLucy <lavrehaludmila@gmail.com>
2026-01-17 17:02:26 +0200

    checking build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,11 @@
-// import {useEffect, useState} from "react";
-// import axios from "axios";
-
 const App = () => {
-    // const [orders, setOrders] = useState<unknown[]>([]);
-    //
-    //
-    // useEffect(() => {
-    //     console.log("Запит запускається")
-    //     axios.get('/api/orders')
-    //         .then(({data}) => {
-    //             setOrders(data);
-    //         })
-    // }, []);
-
     console.log('FRONTEND VERSION 123');
 
-    return(
-            <div>
-                {/*{*/}
-                {/*    orders.map(order => <div key={order.id}> {JSON.stringify(order)}</div>)*/}
-                {/*}*/}
-                <h1>Checking</h1>
-            </div>
-    )
+    return (
+        <div>
+            <h1>Checking</h1>
+        </div>
+    );
 };
-export {App};
+
+export default App;
