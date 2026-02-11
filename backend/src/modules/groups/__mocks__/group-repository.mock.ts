37573466a7cd9dda81544b9a25e27c64d0329982
LavrekhaLucy <lavrehaludmila@gmail.com>
2026-02-11 10:21:57 +0200

    Add groups module with service and controller (backend)+tests

--- backend/src/modules/groups/__mocks__/group-repository.mock.ts
+++ backend/src/modules/groups/__mocks__/group-repository.mock.ts
@@ -3,6 +3,7 @@ import { GroupRepository } from '../../repository/services/group.repository';
 
 export const mockRepositoryGroup: MockServiceType<Partial<GroupRepository>> = {
   create: jest.fn(),
+  save: jest.fn(),
   find: jest.fn(),
   findOne: jest.fn(),
 };
