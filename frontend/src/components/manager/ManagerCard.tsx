8081f18d4ced4fbd2f8aecbb5b69f70608d32659
LavrekhaLucy <lavrehaludmila@gmail.com>
2026-03-17 17:56:29 +0200

    Feat: prevent admins from banning themselves in UI and backend

--- frontend/src/components/manager/ManagerCard.tsx
+++ frontend/src/components/manager/ManagerCard.tsx
@@ -1,7 +1,8 @@
 import {type FC, useState} from "react";
 import type {IUser} from "../../models/interfaces/IUser/IUser.ts";
-import {useAppDispatch, useAppSelector} from "../store/store.ts";
+import {useAppDispatch} from "../store/store.ts";
 import {adminActions} from "../../slices/adminSlice.ts";
+import {useAuth} from "../hooks/useAuth.ts";
 
 type State = {
     user: IUser ;
@@ -11,9 +12,10 @@ export const ManagerCard: FC<State> = ({ user }) => {
     const [isCopied, setIsCopied] = useState(false);
     const dispatch = useAppDispatch();
 
-    const { me } = useAppSelector(state => state.authStoreSlice);
+    const { me, loading } = useAuth();
 
-    const isSelf = me?.id === user.id;
+    const isSelf = !loading && me?.id === user.id;
+
 
 
     const handleCopyLink = (userId: number) => {
@@ -30,7 +32,6 @@ export const ManagerCard: FC<State> = ({ user }) => {
 
 
     return (
-        // <div className="border-2 border-green-600 rounded-xl p-4 flex justify-between items-start">
 
             <div className={`border-2 rounded-xl p-4 flex justify-between items-start transition-all ${
                 isSelf ? "border-blue-400 bg-blue-50/10" : "border-green-600"
@@ -69,10 +70,9 @@ export const ManagerCard: FC<State> = ({ user }) => {
                     <div className="min-w-35 flex justify-end">
                         <button
                             onClick={() => handleCopyLink(user.id)}
-                            disabled={isSelf}
-                            // className="w-fit whitespace-nowrap bg-green-600 text-white px-3 py-1 rounded text-xs uppercase"
+                            disabled={loading || isSelf}
                             className={`w-fit whitespace-nowrap px-3 py-1 rounded text-xs uppercase transition-colors ${
-                                isSelf
+                                !loading && isSelf
                                     ? "bg-gray-300 text-gray-500 cursor-not-allowed"
                                     : "bg-green-600 text-white hover:bg-green-700"
                             }`}
@@ -88,9 +88,9 @@ export const ManagerCard: FC<State> = ({ user }) => {
 
                     <button
                         onClick={() => handleToggleBan(user.id, 'ban')}
-                        disabled={!user.isActive || isSelf}
+                        disabled={loading || isSelf || !user.isActive}
                         className={`px-3 py-1 rounded text-xs uppercase transition-colors ${
-                            user.isActive && !isSelf
+                            !loading && !isSelf && user.isActive
                                 ? "bg-green-600 text-white px-3 py-1 rounded text-xs uppercase"
                                 : "bg-gray-300 text-gray-500 cursor-not-allowed"
                         }`}
@@ -102,9 +102,9 @@ export const ManagerCard: FC<State> = ({ user }) => {
 
                     <button
                         onClick={() => handleToggleBan(user.id, 'unban')}
-                        disabled={user.isActive || isSelf}
+                        disabled={loading || isSelf || user.isActive}
                         className={`px-3 py-1 rounded text-xs uppercase transition-colors ${
-                            !user.isActive && !isSelf
+                            !loading && !isSelf && !user.isActive
                                 ? "bg-green-600 text-white hover:bg-green-700"
                                 : "bg-gray-300 text-gray-500 cursor-not-allowed"
                         }`}
